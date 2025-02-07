116f8a9845cdb83e1e388cbbf37f4ffebb1a5a25
isakatirci <ce.isakatirci@gmail.com>
2025-02-07 04:14:46 +0300

    fix

--- src/main/java/Race/Condition/Demo/Project/CreditCardRepository.java
+++
@@ -1,8 +0,0 @@
-package Race.Condition.Demo.Project;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface CreditCardRepository extends JpaRepository<CreditCardTransaction, Long> {
-}
