11a8d55ec689a0ca9d19854f331d911b9acc540c
isakatirci <ce.isakatirci@gmail.com>
2025-02-07 04:10:47 +0300

    fix

--- src/main/java/Race/Condition/Demo/Project/CreditCardTransaction.java
+++ src/main/java/Race/Condition/Demo/Project/CreditCardTransaction.java
@@ -11,18 +11,10 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@Table(name = "creditCardTransaction")
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
 public class CreditCardTransaction {
-    @Id
     private Long id;
-    @Column(name = "amount")
     private int amount;
-    @Column(name = "productName")
     private String productName;
-    @Column(name = "customerFirstName")
     private String customerFirstName;
 }
 
